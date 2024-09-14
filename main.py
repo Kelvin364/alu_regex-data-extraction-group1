8b954a63cb05060109c6a6dacf69328f6b31ddf1
Blandin3 <b.iradukund3@alustudent.com>
2024-09-14 14:00:01 +0000

    Added Extracting URLs Regex

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ def extract_Emails(Emails):
     Emails_pattern = r'\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b'
     return re.findall(Emails_pattern, Emails)
 
-
+# 2. Extracting URLs
+def extract_urls(text):
+    url_pattern = r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'
+    return re.findall(url_pattern, text)
 
 
 # Main function to load the text and run the extractions
@@ -23,6 +26,9 @@ def main():
     print()
     print("Extracted Emails:", extract_Emails(sample_text))
     print()
+    print("Extracted URLs:", extract_urls(sample_text))
+    print()
+
  
   
 
