b3e53e0d65bcf0aba6815ec09bf9f620461f1b67
Codedfucha <a.gichure@alustudent.com>
2024-09-15 06:05:25 -0700

    Added function to extract HTML tags

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ def extract_urls(text):
     url_pattern = r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'
     return re.findall(url_pattern, text)
 
+# 3. Extract HTML_Tags
+def extract_html_tags(text):
+    html_pattern = r'<[^>]+>'
+    return re.findall(html_pattern, text)
 
 # Main function to load the text and run the extractions
 def main():
@@ -27,9 +31,7 @@ def main():
     print("Extracted Emails:", extract_Emails(sample_text))
     print()
     print("Extracted URLs:", extract_urls(sample_text))
-    print()
-
- 
+    print("Extracted HTML Tags:", extract_html_tags(sample_text))
   
 
 if __name__ == '__main__':
